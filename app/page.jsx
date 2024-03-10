afe93611e686a3a535a8a0a9dd784f9ab1a1bb83
Daniel Enriquez <cdeh88@gmail.com>
2024-03-10 00:14:07 -0600

    add dashboard and change file extensions

--- app/page.jsx
+++ app/page.jsx
@@ -5,8 +5,8 @@ export default function Home() {
     <div className='flex min-h-screen flex-col p-2 bg-black gap-2'>
       <nav className='flex rounded-md h-10 bg-white items-center p-8'>
         <div className='flex items-center gap-2'>
-          <BsShopWindow size={32} />
-          <h1>Compare Shopping</h1>
+          <BsShopWindow size={32} color='rgb(59 130 246);'/>
+          <h1 className='text-blue-500'>Compare Shopping</h1>
         </div>
       </nav>
       <main className='flex gap-2 justify-between w-full text-white'>
